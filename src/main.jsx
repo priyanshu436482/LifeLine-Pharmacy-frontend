d778b990212328858a88c30c407c67414252bce3
priyanshu436482 <patelpinshu4246@gmail.com>
2026-05-15 15:09:50 +0530

    Prepare frontend for new repository

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { CartProvider } from './context/CartContext'
 import { SearchProvider } from './context/SearchContext'
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <SearchProvider>
         <CartProvider>
           <App />
         </CartProvider>
       </SearchProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
